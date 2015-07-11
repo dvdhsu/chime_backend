cc644bfc91e07d9b03845084b11bd25d5bde26a2
David Hsu <dvdhsu@gmail.com>
2015-07-11 19:43:02 +0000

    Add basic address params logging.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var monk = require('monk');
 var db =  monk('localhost:27017/chime');
 var bodyParser = require('body-parser');
 var multer = require('multer');
+var url = require('url');
 
 var eventTestCollection = db.get('crimes');
 
@@ -17,6 +18,21 @@ app.use(multer());
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/echo', function (req, res) {
+  var params = url.parse(req.url, true).query;
+
+  params.start_address // this is your start address
+  params.end_address // ...
+
+  console.log("start address: " + params.start_address);
+  console.log("end address: " + params.end_address);
+
+  // now we somehow need to get directions from the Google Maps
+  // api, then send them back via `res.send`
+
+  res.send(params);
 });
 
 var server = app.listen(3100, function () {
